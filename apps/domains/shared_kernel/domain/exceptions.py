482b32b87f6bc4d11663c925c496e90beb688d49
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-11-03 15:17:25 +0500

    03.11.2025 Refactor shared kernel

--- apps/domains/shared_kernel/domain/exceptions.py
+++
@@ -1,6 +0,0 @@
-class DomainError(Exception):
-    """Базовая доменная ошибка"""
-
-    def __init__(self, message: str, code: str) -> None:
-        super().__init__(message)
-        self.code = code
