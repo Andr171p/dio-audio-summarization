f8bd65592db80ef2a818ad6827fe95e8c216009b
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-11-06 17:59:38 +0500

    .

--- apps/modules/summary/commands.py
+++
@@ -1,9 +0,0 @@
-from uuid import UUID
-
-from pydantic import BaseModel
-
-
-class SummarizeAudioCollectionCommand(BaseModel):
-    """Суммаризовать аудио коллекцию"""
-    collection_id: UUID
-    summary_type: ...
